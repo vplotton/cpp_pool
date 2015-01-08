212d22d19c4c35de28f462b0e188167ee93c94ae
Vianney PLOTTON <vplotton@e3r10p16.42.fr>
2015-01-08 14:24:45 +0100

    D03 -> ex00 - new fresh main :)

--- D03/ex00/main.cpp
+++ D03/ex00/main.cpp
@@ -1,8 +1,39 @@
 #include "FragTrap.hpp"
+#include <iostream>
 
 int	main()
 {
+	FragTrap *noName = new FragTrap();
 	FragTrap fragTrap("Betty");
+
+	std::cout << std::endl;
+	delete noName;
+
+	std::cout << std::endl;
+	fragTrap.vaulthunter_dot_exe("Hector");
+	fragTrap.vaulthunter_dot_exe("Hector");
+	fragTrap.vaulthunter_dot_exe("Hector");
+	fragTrap.vaulthunter_dot_exe("Hector");
+	fragTrap.vaulthunter_dot_exe("Hector");
+	
+	std::cout << std::endl;
+	fragTrap.rangedAttack("Bernie");
+	fragTrap.meleeAttack("Bernie");
+
+	std::cout << std::endl;
+	fragTrap.takeDamage(25);
+	fragTrap.takeDamage(25);
+
+	std::cout << std::endl;
+	fragTrap.beRepaired(25);
+	fragTrap.beRepaired(25);
+
+	std::cout << std::endl;
+	fragTrap.takeDamage(25);
+	fragTrap.takeDamage(25);
+	fragTrap.takeDamage(25);
+	fragTrap.takeDamage(25);
+	fragTrap.takeDamage(25);
 
 	return (0);
 }
