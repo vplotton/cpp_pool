aa27954e429ad6f8da71fbbec764571f32ae0dd7
Cyril CHARBONNIER <cycharbo@e3r11p17.42.fr>
2015-01-11 00:19:27 +0100

    display et control

--- rush00/Display.cpp
+++ rush00/Display.cpp
@@ -1,4 +1,5 @@
 #include "Display.hpp"
+#include <ncurses.h>
 
 Display::Display()
 {
@@ -22,8 +23,54 @@ Display & Display::operator=(Display const & rhs)
 	return (*this);
 }
 
-std::ostream & operator<<(std::ostream & o, Display const & i)
+
+WINDOW * Display::get_win()
 {
-	o << "Don't forget to change this.";
-	return (o);
+	return (_win);
+}
+
+void Display::set_win(WINDOW* win)
+{
+	this->_win = win;
+}
+
+WINDOW*	Display::ft_init_win()
+{
+	WINDOW *win;
+	int height = 50;
+	int width = 100;
+	int starty = 0;
+	int startx = 0;
+
+	win = newwin(height, width, starty, startx);
+	box(win, 0 , 0);
+	wrefresh(win);
+	return (win);
+}
+
+void Display::destroy_win(WINDOW *win)
+{
+	wborder(win, ' ', ' ', ' ',' ',' ',' ',' ',' ');
+	wrefresh(win);
+	delwin(win);
+}
+
+void	Display::print_obj(WINDOW *win, int x, int y, int type)
+{
+	if (type == 0)
+	{
+		mvwaddch(win, y, x, ACS_DIAMOND);
+		//wrefresh(_win);
+	}
+	else if (type == 1)
+	{
+		mvwaddch(win, y, x, ACS_DARROW);
+		wrefresh(_win);
+	}
+	else if (type == 2)
+	{
+		mvwaddch(win, y, x, ACS_BTEE);
+		wrefresh(_win);
+	}
+	
 }
--- rush00/Display.hpp
+++ rush00/Display.hpp
@@ -2,20 +2,25 @@
 # define DISPLAY_HPP
 
 #include <iostream>
+#include <ncurses.h>
 
 class Display
 {
 	public:
-		Display();
-		Display(Display const & src);
-		~Display();
-
-		Display & operator=(Display const & rhs);
+					Display();
+					Display(Display const & src);
+					~Display();
+		WINDOW *	get_win(void);
+		Display & 	operator=(Display const & rhs);
+		void 		set_win(WINDOW *win);
+		void		destroy_win(WINDOW * win);
+		WINDOW*  	ft_init_win(void);
+		void		print_obj(WINDOW *win,int x, int y, int type);
 
 	private:
+					WINDOW* _win;
 
 };
 
-std::ostream & operator<<(std::ostream & o, Display const & i);
 
 #endif /* !DISPLAY_HPP */
