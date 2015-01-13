a0e275981e6fddc13eacaf0ab352f259447829d7
Vianney PLOTTON <vplotton@e3r11p20.42.fr>
2015-01-13 22:01:11 +0100

    D06 -> ex00 - sry no header authorized here

--- D06/ex00/main.cpp
+++ D06/ex00/main.cpp
@@ -1,40 +1,27 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: sbres <sbres@student.42.fr>                +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2015/01/13 16:22:31 by sbres             #+#    #+#             */
-/*   Updated: 2015/01/13 16:22:53 by sbres            ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-#include <iostream>
-#include <stdlib.h>
 #include "Scalar.hpp"
+#include <iostream>
+#include <cstdlib>
 
-int main(int argc, char const *argv[])
+int main(int argc, char const **argv)
 {
-	/* code */
 	double test;
-	char * toto;
+	char * end;
 
-	if (argc == 1)
+	if (argc != 2)
 	{
-		std::cout << "Please specify parameter" << std::endl;
-		return 0;
+		std::cout << "Wrong input" << std::endl;
+		return -1;
 	}
-	test = strtod(argv[1], &toto);
-	if (test == 0 && toto == argv[1])
+	test = strtod(argv[1], &end);
+	if (test == 0 && end == argv[1])
 	{
 		Scalar value1 = Scalar(argv[1][0]);
-		std::cout << value1 <<std::endl;
+		std::cout << value1 << std::endl;
 	}
 	else
 	{
 		Scalar value1 = Scalar(test);
-		std::cout << value1 <<std::endl;
+		std::cout << value1 << std::endl;
 	}
 	return 0;
 }
