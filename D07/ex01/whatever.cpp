fb4de86b80299ba740b1189bf0aa59447b6aedb8
vplotton <vianney.plotton@gmail.com>
2015-01-14 13:49:24 +0100

    D07 -> ex01 - sbres method

--- D07/ex01/whatever.cpp
+++ D07/ex01/whatever.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 
-template <typename T>
-void	iter(T *& array, unsigned int size, void (*ft)(T &))
+static int	count = 0;
+
+template <typename T, typename U>
+void	iter(T *& array, unsigned int size, U (*ft)(T &))
 {
 	for (unsigned int i = 0 ; i < size ; ++i)
 	{
@@ -14,12 +16,30 @@ void	ft_increment(int &a)
 	++a;
 }
 
+template <typename T, typename U>
+U	ft_decrement(T &a)
+{
+	if (count == 1)
+	{
+		std::cout << "U ft_decrement(T &a);" << std::endl;
+		count++;
+	}
+	--a;
+	return (a);
+}
+
 template <typename T>
 void	ft_decrement(T &a)
 {
+	if (count == 0)
+	{
+		std::cout << "void ft_decrement(T &a);" << std::endl;
+		count++;
+	}
 	--a;
 }
-template void ft_decrement<float>(float & a);
+template float ft_decrement<float, float>(float & a);
+
 
 
 int		main()
@@ -34,9 +54,9 @@ int		main()
 	std::cout << std::endl;
 	std::cout << std::endl;
 
-	iter<int>(array, 10, &ft_increment);
+	iter<int, void>(array, 10, &ft_increment);
 
-	std::cout << "[ TEST ] Iter ft_increment <int>" << std::endl;
+	std::cout << "[ TEST ] Iter ft_increment <int, void>" << std::endl;
 	for (int i = 0 ; i < 10 ; ++i)
 	{
 		std::cout << "array[" << i << "] = " << array[i] << "  ";
@@ -56,9 +76,19 @@ int		main()
 	std::cout << std::endl;
 	std::cout << std::endl;
 
-	iter<float>(arrayFloat, 10, &ft_decrement);
 
-	std::cout << "[ TEST ] Iter ft_decrement template <float>" << std::endl;
+	std::cout << "[ TEST ] Iter ft_decrement template <float, void>" << std::endl;
+	iter<float, void>(arrayFloat, 10, &ft_decrement);
+	for (int i = 0 ; i < 10 ; ++i)
+	{   
+		std::cout << "array[" << i << "] = " << arrayFloat[i] << "  ";
+	}   
+	std::cout << std::endl;
+	std::cout << std::endl;
+
+
+	std::cout << "[ TEST ] Iter ft_decrement template <float, float>" << std::endl;
+	iter<float, float>(arrayFloat, 10, &ft_decrement);
 	for (int i = 0 ; i < 10 ; ++i)
 	{   
 		std::cout << "array[" << i << "] = " << arrayFloat[i] << "  ";
