c11a2d72612e474c70c50866d42cfa2ed2068e2f
Vianney PLOTTON <vplotton@e3r9p17.42.fr>
2015-01-14 17:50:20 +0100

    D07 -> ex01 - better main

--- D07/ex01/whatever.cpp
+++ D07/ex01/whatever.cpp
@@ -3,7 +3,7 @@
 static int	count = 0;
 
 template <typename T, typename U>
-void	iter(T *& array, unsigned int size, U (*ft)(T &))
+void	iter(T * const & array, unsigned int size, U (*ft)(T &))
 {
 	for (unsigned int i = 0 ; i < size ; ++i)
 	{
@@ -13,13 +13,18 @@ void	iter(T *& array, unsigned int size, U (*ft)(T &))
 
 void	ft_increment(int &a)
 {
+	if (count == 0)
+	{
+		std::cout << "void ft_increment(int &a);" << std::endl;
+		count++;
+	}
 	++a;
 }
 
-template <typename T, typename U>
+	template <typename T, typename U>
 U	ft_decrement(T &a)
 {
-	if (count == 1)
+	if (count == 0)
 	{
 		std::cout << "U ft_decrement(T &a);" << std::endl;
 		count++;
@@ -28,7 +33,8 @@ U	ft_decrement(T &a)
 	return (a);
 }
 
-template <typename T>
+
+	template <typename T>
 void	ft_decrement(T &a)
 {
 	if (count == 0)
@@ -40,60 +46,100 @@ void	ft_decrement(T &a)
 }
 template float ft_decrement<float, float>(float & a);
 
+std::string	ft_reverse(std::string & s)
+{
+	if (count == 0)
+	{
+		std::cout << "std::string ft_reverse(std::string & s)" << std::endl;
+		count++;
+	}
+
+	std::string copy(s);
+	for (unsigned int i = 0 ; i < s.size() ; ++i)
+	{
+		s.at(i) = copy.at(s.size() - i - 1);
+	}
+	return s;
+}
 
 
 int		main()
 {
-	int	*array = new int[10];
+	int	*array = new int[3];
 	std::cout << std::endl;
-	for (int i = 0 ; i < 10 ; ++i)
+	std::cout << "[ TEST ] Iter ft_increment <int, void>" << std::endl;
+	for (int i = 0 ; i < 3 ; ++i)
 	{
 		array[i] = i;
 		std::cout << "array[" << i << "] = " << array[i] << "  ";
 	}
 	std::cout << std::endl;
-	std::cout << std::endl;
 
-	iter<int, void>(array, 10, &ft_increment);
+	iter<int, void>(array, 3, &ft_increment);
 
-	std::cout << "[ TEST ] Iter ft_increment <int, void>" << std::endl;
-	for (int i = 0 ; i < 10 ; ++i)
+	for (int i = 0 ; i < 3 ; ++i)
 	{
 		std::cout << "array[" << i << "] = " << array[i] << "  ";
 	}
 	std::cout << std::endl;
 	std::cout << std::endl;
-	
-
-
-	float     *arrayFloat = new float[10];
 	std::cout << std::endl;
-	for (int i = 0 ; i < 10 ; ++i)
+
+
+
+	count = 0;
+	std::cout << "[ TEST ] Iter ft_decrement template <float, void>" << std::endl;
+	float     *arrayFloat = new float[10];
+	for (int i = 0 ; i < 3 ; ++i)
 	{   
 		arrayFloat[i] = static_cast<float>(i) / 2;
 		std::cout << "array[" << i << "] = " << arrayFloat[i] << "  ";
 	}   
 	std::cout << std::endl;
-	std::cout << std::endl;
 
-
-	std::cout << "[ TEST ] Iter ft_decrement template <float, void>" << std::endl;
-	iter<float, void>(arrayFloat, 10, &ft_decrement);
-	for (int i = 0 ; i < 10 ; ++i)
+	iter<float, void>(arrayFloat, 3, &ft_decrement);
+	for (int i = 0 ; i < 3 ; ++i)
 	{   
 		std::cout << "array[" << i << "] = " << arrayFloat[i] << "  ";
 	}   
 	std::cout << std::endl;
 	std::cout << std::endl;
+	std::cout << std::endl;
 
 
+	count = 0;
 	std::cout << "[ TEST ] Iter ft_decrement template <float, float>" << std::endl;
-	iter<float, float>(arrayFloat, 10, &ft_decrement);
-	for (int i = 0 ; i < 10 ; ++i)
+	for (int i = 0 ; i < 3 ; ++i)
+	{
+		std::cout << "array[" << i << "] = " << arrayFloat[i] << "  ";
+	}
+	std::cout << std::endl;
+	iter<float, float>(arrayFloat, 3, &ft_decrement);
+	for (int i = 0 ; i < 3 ; ++i)
 	{   
 		std::cout << "array[" << i << "] = " << arrayFloat[i] << "  ";
 	}   
 	std::cout << std::endl;
 	std::cout << std::endl;
+	std::cout << std::endl;
 
+
+	count = 0;
+	std::cout << "[ TEST ] Iter ft_reverse template <std::string, std::string>"	<< std::endl;
+	std::string	*arrayString = new std::string[3];
+	arrayString[0] = "blabla";
+	arrayString[1] = "blibli";
+	arrayString[2] = "bloblo";
+	for (int i = 0 ; i < 3 ; ++i)
+	{
+		std::cout << "arrayString[" << i << "] = " << arrayString[i] << std::endl;
+	}
+
+	iter<std::string, std::string>(arrayString, 3, &ft_reverse);
+	for (int i = 0 ; i < 3 ; ++i)
+	{
+		std::cout << "array[" << i << "] = " << arrayString[i] << std::endl;
+	}
+	std::cout << std::endl;
+	std::cout << std::endl;
 }
