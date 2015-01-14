de37904a1bee65e04093f5d48290fec14bc7cfb4
Vianney PLOTTON <vplotton@e3r9p17.42.fr>
2015-01-15 00:51:46 +0100

    D07 -> ex02 - yet a better main

--- D07/ex02/main.cpp
+++ D07/ex02/main.cpp
@@ -56,51 +56,51 @@ int		main()
 	std::cout << std::endl;
 	std::cout << std::endl;
 
-	std::cout << "==== Array Of 10 int ====" << std::endl;
-	Array<int>	size10Array(10);
-	std::cout << "size: " << size10Array.size() << std::endl;
-	std::cout << "Array[0] = " << size10Array[0] << std::endl;
-	std::cout << "Array[10] = ";
+	std::cout << "==== Array Of 3 <int> ====" << std::endl;
+	Array<int>	size3Array(3);
+	std::cout << "size: " << size3Array.size() << std::endl;
+	std::cout << "Array[0] = " << size3Array[0] << std::endl;
+	std::cout << "Array[3] = ";
 	try
 	{
-		std::cout << size10Array[10];
+		std::cout << size3Array[3];
 	}
 	catch (std::out_of_range &e)
 	{
 		std::cerr << e.what() << std::endl;
 	}
-	std::cout << "Array[5] = 5" << std::endl;
-	size10Array[5] = 5;
-	for (unsigned int i = 0 ; i < size10Array.size() ; ++i)
+	std::cout << "Set Array[2] = 2" << std::endl;
+	size3Array[2] = 2;
+	for (unsigned int i = 0 ; i < size3Array.size() ; ++i)
 	{
-		std::cout << "size10Array[" << i << "] = " << size10Array[i] << std::endl;
+		std::cout << "size3Array[" << i << "] = " << size3Array[i] << std::endl;
 	}
 	std::cout << std::endl;
 	std::cout << std::endl;
 
 
-	std::cout << "==== Copy Of size10Array int ====" << std::endl;
-	Array<int>   size10CopyArray(size10Array);
-	std::cout << "size: " << size10CopyArray.size() << std::endl;
-	std::cout << "Array[0] = " << size10CopyArray[0] << std::endl;
-	std::cout << "Array[10] = ";
+	std::cout << "==== Copy Of size3Array <int> ====" << std::endl;
+	Array<int>   size3CopyArray(size3Array);
+	std::cout << "size: " << size3CopyArray.size() << std::endl;
+	std::cout << "Array[0] = " << size3CopyArray[0] << std::endl;
+	std::cout << "Array[3] = ";
 	try 
 	{   
-		std::cout << size10CopyArray[10];
+		std::cout << size3CopyArray[3];
 	}   
 	catch (std::out_of_range &e) 
 	{   
 		std::cerr << e.what() << std::endl;
 	} 
-	for (unsigned int i = 0 ; i < size10CopyArray.size() ; ++i)
+	for (unsigned int i = 0 ; i < size3CopyArray.size() ; ++i)
 	{
-		std::cout << "size10CopyArray[" << i << "] = " << size10CopyArray[i] << std::endl;
+		std::cout << "size3CopyArray[" << i << "] = " << size3CopyArray[i] << std::endl;
 	}
 	std::cout << std::endl;
 	std::cout << std::endl;
 
 
-	std::cout << "==== StringArray int ====" << std::endl;
+	std::cout << "==== StringArray <std::string> ====" << std::endl;
 	Array<std::string>	stringArray(3);
 	std::cout << "size: " << stringArray.size() << std::endl;
 	stringArray[0] = "blabla";
@@ -118,7 +118,57 @@ int		main()
 	catch (std::out_of_range &e) 
 	{   
 		std::cerr << e.what() << std::endl;
-	} 
+	}
+	std::cout << std::endl;
+	std::cout << std::endl;
+
+
+	std::cout << "==== intPointerArray <int*> ====" << std::endl;
+	Array<int*>  intPointerArray(3);
+	std::cout << "size: " << intPointerArray.size() << std::endl;
+	int		intTab[3];
+	intTab[0] = 1;
+	intTab[1] = 2;
+	intTab[2] = 3;
+	intPointerArray[0] = &intTab[0];
+	intPointerArray[1] = &intTab[1];
+	intPointerArray[2] = &intTab[2];
+	for (unsigned int i = 0 ; i < intPointerArray.size() ; ++i)
+	{
+		std::cout << "intPointerArray[" << i << "] = " << intPointerArray[i]
+			<< std::endl;
+	}
+	std::cout << "intPointerArray[10] = ";
+	try
+	{
+		std::cout << intPointerArray[10];
+	}
+	catch (std::out_of_range &e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+	std::cout << std::endl;
+	std::cout << std::endl;
+
+	std::cout << "==== arrayArray <Array<std::string> > ====" << std::endl;
+	Array<Array<std::string> >  arrayArray(1);
+	arrayArray[0] = stringArray;
+	std::cout << "Set arrayArray[0] = stringArray" << std::endl;
+	std::cout << "size: " << arrayArray.size() << std::endl;
+	for (unsigned int i = 0 ; i < arrayArray[0].size() ; ++i)
+	{
+		std::cout << "arrayArray[0][" << i << "] = "
+			<< arrayArray[0][i] << std::endl;
+	}
+	std::cout << "arrayArray[1] = ";
+	try
+	{
+		arrayArray[1] = stringArray;
+	}
+	catch (std::out_of_range &e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
 #endif
 	return 0;
 }
