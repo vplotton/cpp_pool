2994a4e40c2153ae56a066adf78b1b166c52691b
Vianney PLOTTON <vplotton@e3r11p18.42.fr>
2015-01-16 23:59:21 +0100

    rush01 -> sysinfo

--- rush01/src/main.cpp
+++ rush01/src/main.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
+#include <sys/utsname.h>
+
+struct utsname unameData;
 
 int		main()
 {
 	std::cout << "Welcome to GKrellM" << std::endl;
+#if defined(__APPLE__) && defined(__MACH__)
+	std::cout << "MAX OS X" << std::endl;
+#endif
+
+	uname(&unameData);
+
+	std::cout << "Sysname: " << unameData.sysname << std::endl;
+	std::cout << "Nodename: " << unameData.nodename << std::endl;
+	std::cout << "Release: " << unameData.release << std::endl;
+	std::cout << "Version: " << unameData.version << std::endl;
+	std::cout << "Machine: " << unameData.machine << std::endl;
 
 	return 0;
 }
